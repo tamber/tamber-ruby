6161fc66e8aa28abccf0d36c6216047256272f73
Alexander Robbins <alexi@tamber.com>
2016-02-15 15:04:50 -0800

    added user and behavior tests

---
+++ test/behavior_test.rb
@@ -0,0 +1,29 @@
+require File.expand_path('../test_helper', __FILE__)
+
+module Tamber
+  class BehaviorTest < Test::Unit::TestCase
+    should "be creatable" do
+      begin
+        e = Tamber::Behavior.create(
+          :name => 'star',
+          :desirability => 0.2
+        )
+        puts e
+      rescue TamberError => error
+        puts error.message
+      end
+    end
+
+    should "be retrievable" do
+      begin
+        e = Tamber::Behavior.retrieve(
+          :name => 'star',
+        )
+        puts e
+      rescue TamberError => error
+        puts error.message
+      end
+
+    end
+  end
+end
